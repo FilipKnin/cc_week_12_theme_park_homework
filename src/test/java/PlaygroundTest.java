cfc8910a00a15116641224070a48928a56814e21
Filip Kuszenin <filipkuszenin@gmail.com>
2018-10-16 23:03:22 +0100

    playground.isAllowedTo overwrited

--- src/test/java/PlaygroundTest.java
+++ src/test/java/PlaygroundTest.java
@@ -6,14 +6,22 @@ import static org.junit.Assert.assertEquals;
 public class PlaygroundTest {
 
     Playground playground;
+    Visitor visitor1;
 
     @Before
     public void before() {
+        visitor1 = new Visitor(12, 180, 240.00);
         playground = new Playground("Playground");
     }
 
     @Test
     public void hasName() {
         assertEquals("Playground", playground.getName());
+    }
+
+    @Test
+    public void hasIsAllowed() {
+        assertEquals(true, playground.isAllowedTo(visitor1));
+
     }
 }
