f04c483ccfb5072509f25cd20cf4e1c04af03997
Filip Kuszenin <filipkuszenin@gmail.com>
2018-10-16 21:17:58 +0100

    TobaccoStall class created  and tested

--- src/test/java/TobaccoStallTest.java
+++ src/test/java/TobaccoStallTest.java
@@ -1,2 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TobaccoStallTest {
+
+    TobaccoStall tobaccoStall;
+
+    @Before
+    public void before() {
+        tobaccoStall = new TobaccoStall("name", "ownerName");
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("name", tobaccoStall.getName());
+    }
+
+    @Test
+    public void hasOwnerName() {
+        assertEquals("ownerName", tobaccoStall.getOwnerName());
+    }
+
+    @Test
+    public void countParkingSpots() {
+        assertEquals(0, tobaccoStall.countParkingSpots());
+    }
 }
