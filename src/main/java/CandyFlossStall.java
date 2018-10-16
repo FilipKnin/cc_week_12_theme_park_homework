56b4d5e098c2254750caf3ee78e326ea35de608c
Filip Kuszenin <filipkuszenin@gmail.com>
2018-10-16 21:16:14 +0100

    CandyFlossStall class created  and tested

--- src/main/java/CandyFlossStall.java
+++ src/main/java/CandyFlossStall.java
@@ -1,2 +1,6 @@
-public class CandyFlossStall {
+public class CandyFlossStall extends Stall {
+
+    public CandyFlossStall(String name, String ownerName) {
+        super(name, ownerName);
+    }
 }
--- src/test/java/CandyFlossStallTest.java
+++ src/test/java/CandyFlossStallTest.java
@@ -1,2 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CandyFlossStallTest {
+
+    CandyFlossStall candyFlossStall;
+
+    @Before
+    public void before() {
+        candyFlossStall = new CandyFlossStall("name", "ownerName");
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("name", candyFlossStall.getName());
+    }
+
+    @Test
+    public void hasOwnerName() {
+        assertEquals("ownerName", candyFlossStall.getOwnerName());
+    }
+
+    @Test
+    public void countParkingSpots() {
+        assertEquals(0, candyFlossStall.countParkingSpots());
+    }
 }
