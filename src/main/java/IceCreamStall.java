e51ba4bebd87c972c6c62563ea5e5155ee6fe388
Filip Kuszenin <filipkuszenin@gmail.com>
2018-10-17 00:04:21 +0100

    IceCreamStall.defaultPrice/PriceFor overwritten

--- src/main/java/IceCreamStall.java
+++ src/main/java/IceCreamStall.java
@@ -1,6 +1,16 @@
-public class IceCreamStall extends Stall {
+public class IceCreamStall extends Stall implements ITicketed {
 
     public IceCreamStall(String name, String ownerName) {
         super(name, ownerName);
+    }
+
+    @Override
+    public double defaultPrice() {
+        return 2.80;
+    }
+
+    @Override
+    public double priceFor(Visitor visitor) {
+        return 2.80;
     }
 }
--- src/test/java/IceCreamStallTest.java
+++ src/test/java/IceCreamStallTest.java
@@ -6,10 +6,12 @@ import static org.junit.Assert.assertEquals;
 public class IceCreamStallTest {
 
     IceCreamStall iceCreamStall;
+    Visitor visitor1;
 
     @Before
     public void before() {
         iceCreamStall = new IceCreamStall("name", "ownerName");
+        visitor1 = new Visitor(13, 150, 240.00);
     }
 
     @Test
@@ -25,5 +27,15 @@ public class IceCreamStallTest {
     @Test
     public void countParkingSpots() {
         assertEquals(0, iceCreamStall.countParkingSpots());
+    }
+
+    @Test
+    public void hasDefaultPrice() {
+        assertEquals(2.80, iceCreamStall.defaultPrice(), 0.01);
+    }
+
+    @Test
+    public void hasPriceForVisitor() {
+        assertEquals(2.80, iceCreamStall.priceFor(visitor1), 0.01);
     }
 }
