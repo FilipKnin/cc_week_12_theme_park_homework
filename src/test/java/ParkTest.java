e2ebce16bd06e27d80155960aea71624771e0ff9
Filip Kuszenin <filipkuszenin@gmail.com>
2018-10-16 20:56:26 +0100

    Park class created and tested

--- src/test/java/ParkTest.java
+++ src/test/java/ParkTest.java
@@ -1,2 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ParkTest {
+
+    Park park;
+
+    @Before
+    public void before() {
+        park = new Park("park");
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("park", park.getName());
+    }
 }
