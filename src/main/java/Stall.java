82f36b2fb324160b5a4bb8694210c2aace1b4db8
Filip Kuszenin <filipkuszenin@gmail.com>
2018-10-16 21:02:39 +0100

    Stall class created and tested

---
+++ src/main/java/Stall.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public abstract class Stall {
+
+    private String name;
+    private String ownerName;
+    private ArrayList<String> parkingSpot;
+
+    public Stall(String name, String ownerName) {
+        this.name = name;
+        this.ownerName = ownerName;
+        this.parkingSpot = new ArrayList<>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getOwnerName() {
+        return this.ownerName;
+    }
+
+    public ArrayList<String> getParkingSpot() {
+        return this.parkingSpot;
+    }
+}
